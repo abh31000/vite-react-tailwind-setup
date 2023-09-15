316e1e20d31d2a8a724268877e7bed5a862e4032
abh31000 <abdelkhalekbouklihacene67@gmail.com>
2023-09-15 16:59:14 +0100

    Improve output

--- templatereact.sh
+++ templatereact.sh
@@ -10,25 +10,31 @@ read appname
 if [ -z "$appname" ] || [ -d "$appname" ]
 then
 	appname="react-app-"$(tr -dc 'a-f0-9' < /dev/urandom | head -c7)
-        echo  "${YELLOW_BOLD}!${CLEAR}${YELLOW} '$appname' is set as your App's name ${CLEAR}"
+        echo  "${YELLOW_BOLD}[!] '$appname' is set as your App's name ${CLEAR}"
 fi
-npm create vite@latest $appname -- --template react-ts
-# npm create vite@latest $appname -- --template react-ts > '/dev/null' 2>&1
+
+npm create vite@latest $appname -- --template react-ts > '/dev/null' 2>&1
+projdir=$(readlink -f .)
+echo "\n${BOLD}Scaffolding project in '${projdir}/$appname/'${CLEAR}\n${GREEN_BOLD}[✓] Done${CLEAR}\n"
+
+# Installing dependencies
+echo "\n${BOLD}Installing dependencies ...${CLEAR}"
 cd $appname
 npm install
+
+# Installing and setting up Tailwind CSS
+echo "\n\n${BOLD}Setting up tailwindcss for you app ...${CLEAR}"
 npm install tailwindcss postcss autoprefixer
 npx tailwindcss init -p
 
-echo "\n${BOLD}Setting up tailwindcss for you app ...${CLEAR}\n"
-
 # Set up Tailwind's config file
-sed -i "s/content: \[\],/content: [\n   '.index.html',\n '.\/src\/**\/*.{js,ts,jsx,tsx}',\n],/" tailwind.config.js
+sed -i "s/content: \[\],/content: [\n   '.index.html',\n '.\/src\/**\/*.{js,ts,jsx,tsx}',\n],/" tailwind.config.js
 
 # Adding Tailwind directives to index.css
 echo "@tailwind base;\n@tailwind components;\n@tailwind utilities;\n" > src/index.css
 
 # Deleting Vite boilerplate code and testing TailwindCSS
 echo "export default function App() {\n   return(\n      <h1 className='text-3xl font-bold underline'> Hello World! </h1>\n   );\n}" > src/App.tsx
-
 rm src/App.css
-echo  "${GREEN}✓${CLEAR}${GREEN_BOLD} App is set up and ready${CLEAR}\n"
+
+echo  "\n\n${GREEN_BOLD}[✓] App is set up and ready${CLEAR}\n"
